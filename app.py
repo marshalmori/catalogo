30bd6b643a949db730ba7549a5afd59f83fb8419
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-06-26 08:40:23 -0300

    Criação da API

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
+
+# Create category with curl
+# curl -X POST "http://localhost:5000/category/api?category_name=MARSHAL&category_description=CAVALHEIRO"
 
 app = Flask(__name__)
 
@@ -9,6 +12,116 @@ engine = create_engine('sqlite:///catalogo.db', connect_args={'check_same_thread
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+# ======== Start API Endpoint ===================
+@app.route('/user/api', methods=['GET', 'POST'])
+def userFunction():
+    if request.method == 'GET':
+        return getAllUsers()
+    if request.method == 'POST':
+        username = request.args.get('username', '')
+        email = request.args.get('email', '')
+        picture = request.args.get('picture', '')
+        return newUser(username, email, picture)
+
+def getAllUsers():
+    users = session.query(User).all()
+    return jsonify(Users=[i.serialize for i in users])
+
+def newUser(username, email, picture):
+    user = User(username = username, email = email, picture = picture)
+    session.add(user)
+    session.commit()
+    return jsonify(User=user.serialize)
+
+
+@app.route('/user/api/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def userFunctionId(user_id):
+    if request.method == 'GET':
+        return getUser(user_id)
+    if request.method == 'PUT':
+        username = request.args.get('username')
+        email = request.args.get('email')
+        picture = request.args.get('picture')
+        return updateUser(user_id, username, email, picture)
+    if request.method == 'DELETE':
+        return deleteUser(user_id)
+
+def getUser(user_id):
+    user = session.query(User).filter_by(id = user_id).one()
+    return jsonify(User=user.serialize)
+
+def updateUser(user_id, username, email, picture):
+    updatedUser = session.query(User).filter_by(id = user_id).one()
+    if username:
+        updatedUser.username = username
+    if email:
+        updatedUser.email = email
+    if picture:
+        updatedUser.picture = picture
+    session.add(updatedUser)
+    session.commit()
+    return 'Usuário de id: %s atualizado com sucesso' % user_id
+
+def deleteUser(user_id):
+    deletedUser = session.query(User).filter_by(id=user_id).one()
+    session.delete(deletedUser)
+    session.commit()
+    return 'Usuário de id: %s foi excluido com sucesso.' % user_id
+
+@app.route('/category/api', methods=['GET', 'POST'])
+def categoryFunction():
+    if request.method == 'GET':
+        return getAllCategories()
+    elif request.method == 'POST':
+        category_name = request.args.get('category_name', '')
+        category_description = request.args.get('category_description', '')
+        # Tem que capturar o id do usuário e passar para o user_id
+        return makeNewCategory(category_name, category_description)
+
+def getAllCategories():
+    categories = session.query(Category).all()
+    return jsonify(Categories=[i.serialize for i in categories])
+
+def makeNewCategory(category_name, category_description):
+    category = Category(category_name = category_name,
+                        category_description = category_description)
+    session.add(category)
+    session.commit()
+    return jsonify(Category=category.serialize)
+
+@app.route('/category/api/<int:category_id>', methods=['GET', 'PUT', 'DELETE'])
+def categoryFunctionId(category_id):
+    if request.method == 'GET':
+        return getCategory(category_id)
+    if request.method == 'PUT':
+        category_name = request.args.get('category_name', '')
+        category_description = request.args.get('category_description', '')
+        return updateCategory(category_id, category_name, category_description)
+    elif request.method == 'DELETE':
+        return delCategory(category_id)
+
+def getCategory(category_id):
+    category = session.query(Category).filter_by(id = category_id).one()
+    return jsonify(Category=category.serialize)
+
+def updateCategory(category_id, category_name, category_description):
+    category = session.query(Category).filter_by(id = category_id).one()
+    if category_name:
+        category.category_name = category_name
+    if category_description:
+        category.category_description = category_description
+    session.add(category)
+    session.commit()
+    return 'Categoria alterada com sucesso %s' % category_id
+
+def delCategory(category_id):
+    deletedCategory = session.query(Category).filter_by(id = category_id).one()
+    session.delete(deletedCategory)
+    session.commit()
+    return 'Categoria de id: %s excluída com sucesso' % category_id
+
+# ======== End API Endpoint ===================
 
 @app.route('/category/login')
 def login():
@@ -35,10 +148,10 @@ def newCategory():
 def editCategory(category_id):
     editedCategory = session.query(Category).filter_by(id=category_id).one()
     if request.method == 'POST':
-        if request.form['name']:
-            editedCategory.category_name = request.form['name']
+        if request.form['category_name']:
+            editedCategory.category_name = request.form['category_name']
         if request.form['category_description']:
-            editedCategory.category_name = request.form['category_description']
+            editedCategory.category_description = request.form['category_description']
         session.add(editedCategory)
         session.commit()
         return redirect(url_for('showCategory'))
@@ -69,8 +182,8 @@ def newItem(category_id):
     if request.method == 'POST':
         newItem = Item(item_name=request.form['name'],
                        price=request.form['price'],
-                       long_description=request.form['long_description'],
-                       short_description=request.form['short_description'],
+                       item_long_description=request.form['long_description'],
+                       item_short_description=request.form['short_description'],
                        category_id = category_id)
         session.add(newItem)
         session.commit()
@@ -88,9 +201,9 @@ def editItem(category_id, item_id):
         if request.form['price']:
             editedItem.price = request.form['price']
         if request.form['long_description']:
-            editedItem.description = request.form['long_description']
+            editedItem.item_long_description = request.form['long_description']
         if request.form['short_description']:
-            editedItem.description = request.form['short_description']
+            editedItem.item_short_description = request.form['short_description']
         session.add(editedItem)
         session.commit()
         return redirect(url_for('showItem', category_id = category_id))
@@ -113,6 +226,7 @@ def descriptionItem(category_id, item_id):
     categories = session.query(Category).all()
     item = session.query(Item).filter_by(id = item_id).one()
     return render_template('item/description_item.html', category_id = category_id, categories = categories, item_id = item_id, item = item)
+
 
 if __name__ == '__main__':
     app.debug = True
--- database_setup.py
+++ database_setup.py
@@ -16,6 +16,16 @@ class User(Base):
     email = Column(String)
     picture = Column(String)
 
+    @property
+    def serialize(self):
+        '''Return object data in easily serializeable format'''
+        return {
+            'id':       self.id,
+            'username': self.username,
+            'email':    self.email,
+            'picture':  self.picture
+        }
+
 class Category(Base):
     __tablename__ = 'category'
 
@@ -24,6 +34,17 @@ class Category(Base):
     category_description = Column(String(100))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+
+    @property
+    def serialize(self):
+        '''Return object data in easily serializable format'''
+        return {
+            'id':                   self.id,
+            'category_name':        self.category_name,
+            'category_description': self.category_description,
+            'user_id':              self.user_id
+        }
+
 
 class Item(Base):
     __tablename__ = 'item'
@@ -37,6 +58,19 @@ class Item(Base):
     user = relationship(User)
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+
+    @property
+    def serialize(self):
+        '''Return object data in easily serializeable format'''
+        return {
+            'id': self.id,
+            'item_name': self.item_name,
+            'item_long_description': self.item_long_description,
+            'item_short_description': self.item_short_description,
+            'price': self.price,
+            'user_id': self.user_id,
+            'category_id': self.category_id
+        }
 
 
 engine = create_engine('sqlite:///catalogo.db')
