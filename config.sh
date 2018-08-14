51a2962f4f4279293e05bdb0a820c69153c7c707
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2018-08-14 11:21:01 -0300

    Inserindo config.sh

---
+++ config.sh
@@ -0,0 +1,12 @@
+pip install Flask
+pip install sqlalchemy
+pip install passlib
+pip install flask-httpauth
+pip install httplib2
+pip install psycopg2
+pip install psycopg2-binary
+pip install requests
+pip install --upgrade oauth2client
+pip install pep8
+pip install pycodestyle
+pip install flask_wtf
